d7f31aa6d52a0a0a872cccb054c56a26f6108ed4
Alexander Bohatskyi <obogatskiy@icloud.com>
2022-01-23 14:57:54 +0200

    upd task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulEl = document.querySelector('#ingredients');
+
+
+const elements = ingredients.map(ingr => {
+  const itemEL = document.createElement('li')
+  itemEL.classList.add('item')
+  itemEL.textContent = ingr
+  return itemEL
+})
+
+
+
+ulEl.append(...elements)
