e2711d252774fb13ecbd6f72b98229848eabea64
Alexander Bohatskyi <obogatskiy@icloud.com>
2022-01-25 09:51:37 +0200

    upd task 05

---
+++ src/js/task-05.js
@@ -0,0 +1,9 @@
+const inputEl = document.querySelector('#name-input')
+const outputEl = document.querySelector('#name-output')
+
+inputEl.addEventListener('input', (event) => {
+    outputEl.textContent = event.currentTarget.value
+    if (inputEl.value === "") {
+        outputEl.textContent = "Anonymous"
+    }
+})
