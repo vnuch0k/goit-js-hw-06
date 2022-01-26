5b336ce352a975c29c17d0c5fdd91f56c9125ffd
Alexander Bohatskyi <obogatskiy@icloud.com>
2022-01-26 19:10:51 +0200

    upd 09 task

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEL = document.querySelector('button')
+const buttonText = document.querySelector('.color')
+const bodyEl = document.querySelector('body')
+
+buttonEL.addEventListener('click', () => {
+  bodyEl.style.backgroundColor = getRandomHexColor()
+  buttonText.textContent = bodyEl.style.backgroundColor
+})
+
