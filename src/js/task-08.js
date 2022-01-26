f6b3de9e861d8450ed380225143c324d61dc2bd1
Alexander Bohatskyi <obogatskiy@icloud.com>
2022-01-26 13:06:59 +0200

    Upd task 08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,27 @@
+const form = document.querySelector('.login-form')
+const buttonEl = document.querySelector('button')
+const inputs = document.querySelectorAll('input')
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    const formElements = event.currentTarget.elements;
+    const mail = formElements.email.value
+    const password = formElements.password.value
+    if (mail === '' || password === '') {
+        alert('Все поля должны быть заполнены!')
+    } else {
+        const dataObj = {
+            email: mail,
+            password: password
+        }
+        console.log(dataObj);
+         event.currentTarget.reset();
+    }
+   
+})
+
+
+
 
